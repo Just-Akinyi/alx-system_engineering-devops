e03e85a579e2b6999f7c50f6ef005e2944945905
Just-Akinyi <justakinyi1@gmail.com>
2022-10-17 23:23:32 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
+"""Given an Employee ID, returns information
+about his/her TODO list progress.
+"""
 import requests
-import sys
-
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        user = sys.argv[1]
-        url = 'https://jsonplaceholder.typicode.com/'
-        user_req = requests.get('{}users/{}'.format(url, user))
-        name = user_req.json().get('name')
-        if name is not None:
-            req = requests.get('{}todos?userId={}'.format(url, user)).json()
-            done = [x for x in req if x.get('completed') is True]
-            tasks = len(req)
-            print("Employee {} is done with tasks({:d}/{:d}):".format(
-                name, len(done), tasks))
-            for dic in done:
-                print('\t {}'.format(dic.get('title')))
+    try:
+        emp_id = int(argv[1])
+    except ValueError:
+        exit()
+
+    api_url = 'https://jsonplaceholder.typicode.com'
+    user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
+    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
+
+    # User Response
+    res = requests.get(user_uri).json()
+
+    # Name of the employee
+    name = res.get('name')
+
+    # User TODO Response
+    res = requests.get(todo_uri).json()
+
+    # Total number of tasks, the sum of completed and non-completed tasks
+    total = len(res)
+
+    # Number of non-completed tasks
+    non_completed = sum([elem['completed'] is False for elem in res])
+
+    # Number of completed tasks
+    completed = total - non_completed
+
+    # Formatting the expected output
+    str = "Employee {emp_name} is done with tasks({completed}/{total}):"
+    print(str.format(emp_name=name, completed=completed, total=total))
+
+    # Printing completed tasks
+    for elem in res:
+        if elem.get('completed') is True:
+            print('\t', elem.get('title'))
