f61133417447f8f1ab07f9e0ce5e666bfafaf75d
Just-Akinyi <justakinyi1@gmail.com>
2022-10-17 23:16:38 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-'''
-using REST API, for a given employee ID,
-returns information about his/her TODO list progress
-'''
-from sys import argv
 import requests
-from sqlalchemy import true
+import sys
 
-if __name__ == "__main__":
-    user_id = argv[1]
-    users_url = 'https://jsonplaceholder.typicode.com/users/' + user_id
-    todos_url = 'https://jsonplaceholder.typicode.com/todos/'
-    users_data = requests.get(users_url)
-    name = users_data.json().get('name')
 
-    todos_data = requests.get(todos_url)
-    todos_id = todos_data.get('userId')
-    done_tasks = todos_data.get('completed')
-    tasks = len(todos_data)
-    done_count = 0
-    title = todos_data.get('title')
-
-    task_title = []
-
-    for i in todos_data:
-        if todos_id == user_id:
-            if done_tasks == true:
-                task_title.append(i[title])
-            done_count += 1
-
-    print('Employee {} is done with tasks(NUMBER_OF_DONE_TASKS/{}})'.format(name, done_tasks,tasks))
-    for x in task_title:
-          print("\t {}".format(x))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        user = sys.argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        user_req = requests.get('{}users/{}'.format(url, user))
+        name = user_req.json().get('name')
+        if name is not None:
+            req = requests.get('{}todos?userId={}'.format(url, user)).json()
+            done = [x for x in req if x.get('completed') is True]
+            tasks = len(req)
+            print("Employee {} is done with tasks({:d}/{:d}):".format(
+                name, len(done), tasks))
+            for dic in done:
+                print('\t {}'.format(dic.get('title')))
